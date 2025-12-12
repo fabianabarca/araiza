b11ba16817d3be4799ccc829f85bb17f5d5fc765
Fabián Abarca Calderón <ensinergia@gmail.com>
2025-12-12 08:45:38 -0300

    Include generated static site to simplify deployment

---
+++ .output/public/_nuxt/BtarjdHw.js
@@ -0,0 +1,1 @@
+import{r as t}from"./DnhMLqBq.js";import a from"./Dp0nP4u6.js";import{Y as e,C as n,o as s,w as c,A as i}from"./CE0ThWs9.js";import"./iik6CYzq.js";const d={__name:"ProseCaution",setup(p){const o=e();return(r,f)=>(s(),n(a,{color:"error",icon:i(o).ui.icons.caution},{default:c(()=>[t(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
